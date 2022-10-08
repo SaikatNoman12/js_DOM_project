904b1ede63fca52228ccfcd5fda66f5b5fdc61ed
saikatnoman12 <noommaa240@gmail.com>
2022-10-08 14:01:04 +0600

    html text changes

--- js/app.js
+++ js/app.js
@@ -6,29 +6,30 @@ function loadMileStone() {
 
     mileStoneData.innerHTML = mileStone.map(function (element) {
         return `
-        <div class="drop-down"  id="${element._id}" >
-            <div class="flex">
-                <div class="checkbox" >
-                    <input type="checkbox" onclick="checkAndDone(this, ${element._id})">
+            <div class="drop-down"  id="${element._id}" >
+                <div class="flex">
+                    <div class="checkbox" >
+                        <input type="checkbox" onclick="checkAndDone(this, ${element._id})">
+                    </div>
+                    <div class="name" onclick="openModuleMenu(this, ${element._id})">
+                        <label>${element.name} 
+                        <i class="fas fa-chevron-down"></i></label>
+                    </div>
                 </div>
-                <div class="name" onclick="openModuleMenu(this, ${element._id})">
-                    <label>${element.name} 
-                    <i class="fas fa-chevron-down"></i></label>
-                </div>
-            </div>
-
-            <div class="hidden-menu">
-                    ${element.modules.map(function (module) {
+    
+                <div class="hidden-menu">
+                        ${element.modules.map(function (module) {
             return `
-                            <div class="module-item">
-                                <p>${module.name}</p>
-                            </div>`
+                                <div class="module-item">
+                                    <p>${module.name}</p>
+                                </div>`
         }).join('')
             }
+                </div>
             </div>
-        </div>
-        `;
+            `;
     }).join('');
+
 
 }
 
@@ -53,17 +54,15 @@ function openModuleMenu(moduleTitle, id) {
 
     hidden_module.classList.toggle('show');
 
-    imageShow(moduleTitle, id);
+    imageShow(id);
 
 }
 
-function imageShow(moduleProps, id) {
-    const left = moduleProps.parentNode.parentNode.parentNode.parentNode.parentNode.previousElementSibling;
+function imageShow(id) {
 
-
-    const image = left.firstElementChild.firstElementChild;
-    const moduleTitle = left.firstElementChild.nextElementSibling.firstElementChild;
-    const moduleDetail = left.firstElementChild.nextElementSibling.lastElementChild;
+    const image = document.getElementById('img');
+    const moduleTitle = document.getElementById('module-title-child');
+    const moduleDetail = document.getElementById('module-detail');
 
 
     image.style.opacity = '0';
@@ -86,14 +85,16 @@ function checkAndDone(checkbox, id) {
     if (checkbox.checked) {
         courseList.removeChild(item);
         doneList.appendChild(item);
+
     }
     else {
         doneList.removeChild(item);
         courseList.appendChild(item);
     }
-
 }
 
+
 loadMileStone();
+
 
 
